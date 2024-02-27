1fd5dbc9072b58d41987e1a6e2b29d1bf628e763
Ayushs38 <ayushsakhale98@gmail.com>
2024-02-27 13:28:20 +0530

    util classes added for apiError, response and asynHandling

--- todo-list-backend/src/index.js
+++ todo-list-backend/src/index.js
@@ -3,8 +3,16 @@ import dotenv from 'dotenv'
 import connectDB from './db/index.js'
 //    add   "update for importing the dotenv"
 
-// dotenv.config({
-//     path: './env'
-// })
+dotenv.config({
+    path: './env'
+})
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running on ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("Mongo DB connection Failed !!!", error);
+})
---
+++ todo-list-backend/src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+
+
+// this is using promises to resolve the async  for handling asynchronous 
+const asyncHandler = (requestHandler) =>{
+    (req, res, next) =>{
+        Promise.resolve(requestHandler(req, res, next)).catch((err)=> next(err))
+    }
+}
+
+
+
+
+// this is using async try catch block for handling asynchronous 
+const asyncTryHandler = (fn) => async (req, res, next) => {
+    try{
+        await fn(req, res, next)
+    }catch(error){
+        res.status(error.code || 500).json({
+            success:false,
+            message: error.message
+        })
+    }
+}
+
+
+export {asyncHandler, asyncTryHandler};
